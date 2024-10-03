9caf077b89c08b767e20230d37ef51c966fcd57a
JonyDuarte <joaoc84duarte@gmail.com>
2024-10-02 21:09:10 -0300

    v1

---
+++ app.py
@@ -0,0 +1,12 @@
+# lista
+campos_endereco = ["Plano Piloto", "Asa sul", "SQS 202", "Bloco A", "Apartamento 301"]
+
+# delimitador
+delimitador = ", "
+
+# juntar os valores em uma única variável
+endereco = delimitador.join(campos_endereco)
+
+# exibe a variável na tela
+print(f"Endereço: {endereco}")
+
